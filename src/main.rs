09d9b38ebef40a36c68462b6319fadc474ad7c5b
Andrew Crites <acrites@mobiquityinc.com>
2015-12-28 13:48:23 -0500

    Improving speed with multiple threads

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate rand;
+extern crate num_cpus;
 
 use std::io;
 use rand::Rng;
 use std::thread;
+use std::sync::{Arc, Mutex, mpsc};
 
 fn make_selection() -> String {
     loop {
@@ -30,63 +32,80 @@ fn main() {
 
     let selection = make_selection();
 
-    let mut fail = 0;
-    let mut suc = 0;
+    let fail = Arc::new(Mutex::new(0));
+    let suc = Arc::new(Mutex::new(0));
 
-    // thread::spawn(|| {
-        for _ in (0..1_000_000) {
-            let mut doors = ['g', 'g', 'g'];
-            doors[rand::thread_rng().gen_range(0, 3)] = 'c';
+    let (tx, rx) = mpsc::channel();
 
-            let choice = rand::thread_rng().gen_range(0, 3);
+    let cpus = num_cpus::get() - 1;
+    let mut threads = vec![];
 
-            let mut monty;
-            let mut final_choice;
-            // Select the right door
-            loop {
-                monty = rand::thread_rng().gen_range(0, 3);
-                // Monty reveals the other wrong door
-                if choice == monty {
-                    continue;
-                }
+    for _ in (0..cpus) {
+        let (selection, suc, fail, tx) = (selection.clone(), suc.clone(), fail.clone(), tx.clone());
+        threads.push(thread::spawn(move || {
+            for _ in (0..1_000_000 / cpus) {
+                let mut doors = ['g', 'g', 'g'];
+                doors[rand::thread_rng().gen_range(0, 3)] = 'c';
 
-                // Monty does not reveal the car
-                if 'c' == doors[monty] {
-                    continue;
-                }
-                break;
-            }
+                let choice = rand::thread_rng().gen_range(0, 3);
 
-            // we choose to switch!
-            if "w" == selection.trim() {
+                let mut monty;
+                let mut final_choice;
+                // Select the right door
                 loop {
-                    final_choice = rand::thread_rng().gen_range(0, 3);
-
-                    // Don't select the goat that monty revealed
-                    if final_choice == monty {
+                    monty = rand::thread_rng().gen_range(0, 3);
+                    // Monty reveals the other wrong door
+                    if choice == monty {
                         continue;
                     }
 
-                    // We force a switch
-                    if final_choice == choice {
+                    // Monty does not reveal the car
+                    if 'c' == doors[monty] {
                         continue;
                     }
                     break;
                 }
-            }
-            else {
-                // We do not switch
-                final_choice = choice;
-            }
 
-            if 'c' == doors[final_choice] {
-                suc += 1;
-            }
-            else {
-                fail += 1;
-            }
-        }
-    // });
+                // we choose to switch!
+                if "w" == selection.trim() {
+                    loop {
+                        final_choice = rand::thread_rng().gen_range(0, 3);
 
-    println!("suc: {}, fail: {}", suc, fail);
+                        // Don't select the goat that monty revealed
+                        if final_choice == monty {
+                            continue;
+                        }
+
+                        // We force a switch
+                        if final_choice == choice {
+                            continue;
+                        }
+                        break;
+                    }
+                }
+                else {
+                    // We do not switch
+                    final_choice = choice;
+                }
+
+                if 'c' == doors[final_choice] {
+                    let mut suc = suc.lock().unwrap();
+                    *suc += 1;
+                }
+                else {
+                    let mut fail = fail.lock().unwrap();
+                    *fail += 1;
+                }
+            }
+            tx.send((suc, fail));
+        }));
+    }
+
+    for thread in threads {
+        let _ = thread.join();
+    }
+
+    let (suc, fail) = rx.recv().unwrap();
+
+    println!("suc: {:?}, fail: {:?}", suc, fail);
 }
